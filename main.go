d381ab55d98a36f86b374d386301f9ff4ee79c14
pciruelos <pciruelos@yahoo.com>
2022-08-25 22:41:00 +0200

    mongodb

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
@@ -15,6 +19,19 @@ func main() {
 	}
 
 	app := fiber.New()
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017/gomongodbreact"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	coll := client.Database("gomongodbreact").Collection("users")
+	coll.InsertOne(context.TODO(), bson.D{{
+		Key:   "name",
+		Value: "Pablo",
+	}})
+
 	app.Use(cors.New())
 
 	app.Static("/", "./client/dist")
